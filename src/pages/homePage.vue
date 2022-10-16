89c824c9bca740972d2b7cb3f03e7dee1a6f019c
madi <mad1196717@gmail.com>
2022-10-16 11:42:15 +0300

    Добавлена пагинация

--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -7,6 +7,10 @@
             :species="char.species" :gender="char.gender" :imgUrl="char.image" :link="char.url" />
       </div>
 
+      <div class="pagination">
+         <div class="pagination__arrow" @click="fetchChars(pagination.currentPage - 1)">&#129092;</div>
+         <div class="pagination__arrow" @click="fetchChars(pagination.currentPage + 1)">&#129094;</div>
+      </div>
    </div>
 </template>
 
@@ -16,20 +20,31 @@ import Card from '@/components/Card';
 import axios from 'axios';
 
 export default {
-   components: { Card },
+   components: { Card},
    data() {
       return {
          chars: [],
          searchQuery: '',
+         pagination: {
+            currentPage: 1,
+            nextPage: null,
+            prevPage: null
+         },
       }
    },
    methods: {
-      async fetchChars() {
+      async fetchChars(pageNumber) {
          try {
-            const response = await axios.get('https://rickandmortyapi.com/api/character');
+            const response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${pageNumber}`);
             this.chars = response.data.results;
+            this.pag = response.data.info
+            this.pagination = {
+               currentPage: pageNumber ? pageNumber : 1,
+               nextPage: this.pag.nextPage,
+               prevPage: this.pag.prevPage
+            }
          } catch (err) {
-            console.log('Произошла ошибка в async fetchChars!')
+            console.log('Произошла ошибка!')
          }
       },
    },
@@ -50,7 +65,6 @@ export default {
 }
 </script>
 
-
 <style scoped>
 .card__wrapper {
    display: flex;
@@ -67,6 +81,22 @@ export default {
    padding: 10px 15px;
    outline: none;
    font-size: 18px;
+}
+
+.pagination {
+   text-align: center;
+   display: flex;
+   justify-content: center;
+   gap: 30px;
+}
+.pagination__arrow {
+   font-size: 48px;
+   cursor: pointer;
+   transition: all ease .2s;
+}
+
+.pagination__arrow:hover {
+   scale: 1.05;
 }
 </style>
 
